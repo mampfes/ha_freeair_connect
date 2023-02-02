ae8ddf3e0ae16579eca916d8ce3a82affe050850
mampfes <mampfes@users.noreply.github.com>
2023-02-02 19:49:22 +0100

    fix warning in HA 2023.2

--- custom_components/freeair_connect/__init__.py
+++ custom_components/freeair_connect/__init__.py
@@ -32,7 +32,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await hass.async_add_executor_job(shell._fac.fetch)
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
