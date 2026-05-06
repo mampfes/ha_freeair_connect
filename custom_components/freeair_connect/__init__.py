334b1c2dc7b700b524dddfbcb7c06fc2640048b4
Thomas Waldecker <thomas.waldecker@qinno.de>
2026-05-06 10:35:20 +0200

    Fix reload after config update.

--- custom_components/freeair_connect/__init__.py
+++ custom_components/freeair_connect/__init__.py
@@ -113,7 +113,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
+    entry.async_on_unload(entry.add_update_listener(_async_options_updated))
+
     return True
+
+
+async def _async_options_updated(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    await hass.config_entries.async_reload(entry.entry_id)
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
